e4ea9d6fac72c99c1c52ed32b5cc96fa7864f57b
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2019-04-09 13:11:04 +0200

    DB connection and express configuration

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-const fs = require('fs');
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/config');
+const accounts = require('./routes/api/accounts');
 
-fs.readFile('/home/nikola/text.txt', (err, data) => {
-  if (err) return console.error(err);
-  const text = data.toString();
+const app = express();
 
-  return console.log(text);
-});
+// Express config
+app.use(express.json());
+
+// Connect and setup database
+mongoose
+  .connect(config.mongooseURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+// Routes
+app.use('/api/accounts', accounts);
+
+app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
