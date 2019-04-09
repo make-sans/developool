3e23e97b511f5a447e0590f93532621805e3007f
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2019-04-09 13:19:30 +0200

    delete and update account functionality

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
     .then((accounts) => {
       res.json(accounts);
     })
-    .catch(err => res.status(404).json({ accounts: [] }));
+    .catch(() => res.status(404).json({ accounts: [] }));
 });
 
 router.post('/', (req, res) => {
@@ -24,6 +24,30 @@ router.post('/', (req, res) => {
     .then(acc => res.json(acc))
     .catch((err) => {
       res.status(500).json({ success: false });
+      console.log(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Account.findById(req.params.id)
+    .then((account) => {
+      account.remove().then(() => res.json({ success: true }));
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false });
+      console.log(err);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  Account.findById(req.params.id)
+    .then((account) => {
+      account.username: req.body.username || account.username;
+      account.passwordHash: req.body.password || account.passwordHash;
+      account.email: req.body.email || account.email;
+    })
+    .catch((err) => {
+      res.status(404).json({ success: false });
       console.log(err);
     });
 });
