2be5c15576fccfe385961e092a1e548e08757b8f
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2019-04-10 11:56:13 +0200

    Using config package for easier configuration

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/config');
 const accounts = require('./routes/api/accounts');
+const config = require('config');
 
 const app = express();
 
@@ -10,11 +10,11 @@ app.use(express.json());
 
 // Connect and setup database
 mongoose
-  .connect(config.mongooseURI, { useNewUrlParser: true })
+  .connect(config.get('mongooseURI'), { useNewUrlParser: true })
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
 // Routes
 app.use('/api/accounts', accounts);
 
-app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
+app.listen(config.get('port'), () => console.log(`Server started on port ${config.get('port')}`));
