7256d97c767979c16f9ecced44fd2b3638fb3f52
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-10 12:14:29 +0200

    Login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,92 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import TextFieldGroup from "../common/TextFieldGroup";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import { Link } from "react-router-dom";
 
-export default class Login extends Component {
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+  }
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    //this.props.loginUser(user);
+  };
   render() {
+    const { errors } = this.state;
+
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="login">
+        <div className="container">
+          <div className="row align-items-center mt-5">
+            <div className="col-md-7 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">Sign in to your account</p>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="Email Address"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                />
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+
+                <input
+                  type="submit"
+                  value="Log in"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+              <div className="text-center mt-2">
+                <p className="d-inline">Don't have an account? </p>
+                <Link className="text-dark underline" to="/register">
+                  Register here
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+Login.propTypes = {
+  //loginUser: PropTypes.func.isRequired,
+  //auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  //auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    //    loginUser
+  }
+)(Login);
