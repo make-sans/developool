98488abd0eacaf0b07c7b7c44f8d84873e2d5124
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-12 10:14:04 +0200

    added bootstrap navbar

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -6,33 +6,51 @@ class Navbar extends Component {
   navbarLinks() {
     if (this.props.auth.isAuthenticated) {
       return [
-        <li key="secret">
-          <Link to="/protected">Protected</Link>
+        <li key="secret" className="nav-item">
+          <Link className="nav-link" to="/protected">
+            Protected
+          </Link>
         </li>,
-        <li key="signout">
-          <a href="" onClick={this.props.logoutUser}>
+        <li className="nav-item" key="signout">
+          <a href="" className="nav-link" onClick={this.props.logoutUser}>
             Sign out
           </a>
         </li>
       ];
     }
     return [
-      <li key="signin">
-        <Link to="/login">Login</Link>
+      <li className="nav-item" key="signin">
+        <Link className="nav-link" to="/login">
+          Login
+        </Link>
       </li>,
-      <li key="signup">
-        <Link to="/register">Register</Link>
+      <li className="nav-item" key="signup">
+        <Link className="nav-link" to="/register">
+          Register
+        </Link>
       </li>
     ];
   }
   render() {
     return (
-      <nav className="navbar">
-        <div className="container">
-          <Link to="/">
-            <span className="brand">Auth-app</span>
-          </Link>
-          <ul>{this.navbarLinks()}</ul>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+        <Link className="navbar-brand" to="/">
+          4th Sem Project
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">{this.navbarLinks()}</ul>
         </div>
       </nav>
     );
