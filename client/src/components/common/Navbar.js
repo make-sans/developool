cef89cf3e4ba7beb1b180b34bed02d19f618ddb3
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-12 09:33:23 +0200

    navbar with links

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logoutUser } from '../../actions/authActions';
 class Navbar extends Component {
   navbarLinks() {
-    if (this.props.authenticated) {
+    if (this.props.auth.isAuthenticated) {
       return [
-        <li key="secret"><Link to="/secret">Secret</Link></li>,
-        <li key="signout"><Link to="/signout">Sign out</Link></li>
+        <li key="secret">
+          <Link to="/protected">Protected</Link>
+        </li>,
+        <li key="signout">
+          <a href="" onClick={this.props.logoutUser}>
+            Sign out
+          </a>
+        </li>
       ];
     }
     return [
-      <li key="signin"><Link to="/login">Login</Link></li>,
-      <li key="signup"><Link to="/register">Register</Link></li>
+      <li key="signin">
+        <Link to="/login">Login</Link>
+      </li>,
+      <li key="signup">
+        <Link to="/register">Register</Link>
+      </li>
     ];
   }
   render() {
     return (
       <nav className="navbar">
         <div className="container">
-          <Link to="/"><span className="brand">Auth-app</span></Link>
-          <ul>
-            {this.navbarLinks()}
-          </ul>
+          <Link to="/">
+            <span className="brand">Auth-app</span>
+          </Link>
+          <ul>{this.navbarLinks()}</ul>
         </div>
       </nav>
     );
@@ -29,7 +40,10 @@ class Navbar extends Component {
 }
 function mapStateToProps(state) {
   return {
-    authenticated: state.auth.authenticated
+    auth: state.auth
   };
 }
-export default connect(mapStateToProps)(Navbar);
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navbar);
