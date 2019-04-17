e80eb63e4200aebc6cf15cb276ae7067d2d58e17
Stefan Joemagi <stefan.joemagi@gmail.com>
2019-04-17 18:21:35 +0200

    More styling on cards

--- client/src/components/project/Projects.js
+++ client/src/components/project/Projects.js
@@ -12,6 +12,11 @@ export class ListProjects extends Component {
     componentDidMount() {
         this.props.getProjects();
     }
+    // render array into list items, if none found then display None
+    renderList = list => {
+        return list.length > 0 ? (list.map(listItem => (<li key={listItem} className="skill-interest-list-item">{listItem}</li>)))
+            : <div className='font-italic text-muted'>None</div>;
+    }
 
     render() {
         // get the properties
@@ -27,28 +32,33 @@ export class ListProjects extends Component {
         else {
             // map projects to list of projects with markup
             projectsList = projects.map(project => (
-                <div className="project w-30 col-md-4" key={project._id}>
-                    <div className="card m-2">
-                        <div className="card-body">
-                            <h5 className="card-title">{project.title}</h5>
-                            <p className="card-text">{project.publicDescription}</p>
-                            <p>{project.interests}</p>
-                            <p>{project.skills}</p>
-                            <Link to={'/project/'+ project._id} className="btn btn-primary">
-                                Project page
-                            </Link>
-                        </div>
+                <div className="card mb-4 shadow-sm rounded">
+                    <div className="card-body">
+                        <h5 className="card-title">{project.title}</h5>
+                        <p className="card-text">{project.publicDescription}</p>
+                        <p>Skills</p>
+                        <ul className="skill-interest-list">
+                            {this.renderList(project.skills)}
+                        </ul>
+                        <p>Interests</p>
+                        <ul className="skill-interest-list">
+                            {this.renderList(project.interests)}
+                        </ul>
+                        <Link to={'/project/' + project._id} className="btn btn-primary">
+                            Project page
+                        </Link>
                     </div>
                 </div>
+
             ));
         }
         return (
             <div className="project-list">
-                <div className="container-fluid">
-                    <div className="row">
-                        {projectsList}
+                <div className="container-fluid my-4">
+                        <div className="card-columns">
+                            {projectsList}
+                        </div>
                     </div>
-                </div>
             </div>
         );
     }
