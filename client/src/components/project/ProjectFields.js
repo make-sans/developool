d98e1e3c2f4699618257e8bf855b20f51ce15d6f
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-17 11:05:59 +0200

    reusable project fields component

---
+++ client/src/components/project/ProjectFields.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react';
+import TextFieldGroup from '../common/TextFieldGroup';
+import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
+import SuggestInput from '../common/SuggestInput';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const SKILLS = [
+  'C#',
+  'JavaScript',
+  'React.js',
+  'Node.js',
+  'Kotlin',
+  'HTML/CSS'
+];
+const INTERESTS = ['Movies', 'Music', 'Sports', 'Anime', 'Games'];
+
+class ProjectFields extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      publicDescription: '',
+      privateDescription: '',
+      interests: [],
+      skills: [],
+      private: false,
+      errors: {}
+    };
+  }
+  componentDidMount() {
+    if (this.props.project) {
+      const { project } = this.props;
+      console.log('proejct supplied');
+
+      this.setState({
+        title: project.title,
+        publicDescription: project.publicDescription,
+        privateDescription: project.privateDescription,
+        interests: project.interests,
+        skills: project.skills,
+        private: project.private
+      });
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+  onSkillSelected = (e, res) => {
+    if (this.state.skills.indexOf(res.suggestion) === -1) {
+      this.setState({ skills: [...this.state.skills, res.suggestion] });
+    }
+  };
+  onInterestSelected = (e, res) => {
+    if (this.state.interests.indexOf(res.suggestion) === -1) {
+      this.setState({ interests: [...this.state.interests, res.suggestion] });
+    }
+  };
+  removeSkill = (e, i) => {
+    this.setState({
+      skills: this.state.skills.filter(skill => skill !== e.target.innerText)
+    });
+  };
+  removeInterest = (e, i) => {
+    this.setState({
+      interests: this.state.interests.filter(
+        interest => interest !== e.target.innerText
+      )
+    });
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  render() {
+    const { errors } = this.state;
+    const skillList = this.state.skills.map(skill => (
+      <li
+        key={skill}
+        className="skill-interest-list-item"
+        onClick={this.removeSkill}
+      >
+        {skill}
+      </li>
+    ));
+    const interestList = this.state.interests.map(interest => (
+      <li
+        key={interest}
+        className="skill-interest-list-item"
+        onClick={this.removeInterest}
+      >
+        {interest}
+      </li>
+    ));
+    return (
+      <form onSubmit={e => this.props.onSubmit(e, this.state)}>
+        <TextFieldGroup
+          label="Title"
+          placeholder="Project title"
+          name="title"
+          type="text"
+          value={this.state.title}
+          onChange={this.onChange}
+          error={errors.title}
+        />
+        <TextAreaFieldGroup
+          label="Description"
+          placeholder="Description"
+          name="publicDescription"
+          type="text"
+          value={this.state.publicDescription}
+          onChange={this.onChange}
+          error={errors.publicDescription}
+        />
+        <TextAreaFieldGroup
+          label="Private description"
+          placeholder="Private description"
+          name="privateDescription"
+          type="text"
+          value={this.state.privateDescription}
+          onChange={this.onChange}
+          error={errors.privateDescription}
+          info="Only members of the project will see this"
+        />
+
+        <div className="form-group">
+          <label>Skills</label>
+          <SuggestInput
+            suggestions={SKILLS}
+            onSelected={this.onSkillSelected}
+            placeholder={'What skills are needed for this project?'}
+          />
+        </div>
+        <ul className="skill-interest-list">{skillList}</ul>
+        <div className="form-group">
+          <label>Interests</label>
+
+          <SuggestInput
+            suggestions={INTERESTS}
+            onSelected={this.onInterestSelected}
+            placeholder={'What interests are suitable for this project?'}
+          />
+        </div>
+        <ul className="skill-interest-list">{interestList}</ul>
+
+        <div className="form-check">
+          <input
+            onChange={e => {
+              this.onChange({
+                target: {
+                  name: e.target.name,
+                  value: e.target.checked
+                }
+              });
+            }}
+            className="form-check-input"
+            type="checkbox"
+            id="private"
+            name="private"
+            checked={this.state.private}
+          />
+          <label className="form-check-label" htmlFor="private">
+            Make it private
+          </label>
+        </div>
+
+        <input
+          type="submit"
+          value="Submit"
+          className="btn btn-primary btn-block mt-4"
+        />
+      </form>
+    );
+  }
+}
+ProjectFields.propTypes = {
+  errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+export default connect(
+  mapStateToProps,
+  {}
+)(ProjectFields);
