18e04733ee62dfb042e6a08c8d60f9432b9f3b62
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-24 13:29:21 +0200

    landing page

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,26 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Landing page</h1>
+        <div className="row justify-content-center mt-4">
+          <div className="col-7 text-center">
+            <h2 className="display-4">Welcome to [4th-semester-project]</h2>
+            <p className="lead">The palace where ideas meet developers</p>
+          </div>
+        </div>
+        <div className="row justify-content-center mt-5 text-white bg-dark p-3">
+          <div className="col-md-3 text-center">
+            <i class="far fa-lightbulb fa-2x p-3" />
+            <p>Explore ideas</p>
+          </div>
+          <div className="col-md-3 text-center">
+            <i class="fas fa-share-alt fa-2x p-3" />
+            <p>Share your idea</p>
+          </div>
+          <div className="col-md-3 text-center">
+            <i class="fas fa-users fa-2x p-3" />
+            <p>Join a project</p>
+          </div>
+        </div>
       </div>
     );
   }
