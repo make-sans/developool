e5cdc787d760e19c8e081e1327180066f5d96434
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-26 14:52:17 +0200

    initial profile reducer and actions

---
+++ client/src/actions/profileAction.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import {
+    GET_PROFILE,
+    PROFILE_LOADING,
+    CLEAR_CURRENT_PROFILE,
+    GET_ERRORS,
+    SET_CURRENT_USER,
+    GET_PROFILES
+} from "./types";
+
+//get current profile
+export const getCurrentProfile = () => dispatch => {
+    dispatch(setProfileLoading());
+    axios
+        .get("/api/profile")
+        .then(res => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            });
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_PROFILE,
+                payload: {}
+            })
+        );
+};
+
+//profile loading
+export const setProfileLoading = () => {
+    return {
+        type: PROFILE_LOADING
+    };
+};
+
+//get profile by id/username
+export const getProfileById = id => dispatch => {
+    dispatch(setProfileLoading());
+    axios
+        .get(`/api/profile/handle/${id}`)
+        .then(res => {
+            dispatch({
+                type: GET_PROFILE,
+                payload: res.data
+            });
+        })
+        .catch(err =>
+            dispatch({
+                type: GET_PROFILE,
+                payload: null
+            })
+        );
+};
