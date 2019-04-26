16a820007d2a89ac4889ce274581c7c2086275a1
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-26 14:53:42 +0200

    profile component

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { getCurrentProfile, getProfileById } from '../../actions/profileAction'
 
-export default class Profile extends Component {
+class Profile extends Component {
+
+  componentDidMount() {
+    const { id } = this.props.match.params
+    if (id) {
+      console.log('by id')
+      //this.props.getProfileById(id)
+    }
+    else {
+      console.log('current');
+      //this.props.getCurrentProfile()
+    }
+  }
   render() {
     return (
       <div>
@@ -9,3 +24,14 @@ export default class Profile extends Component {
     )
   }
 }
+Profile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  getProfileById: PropTypes.func.isRequired,
+}
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+})
+export default connect(mapStateToProps, { getCurrentProfile, getProfileById })(Profile)
