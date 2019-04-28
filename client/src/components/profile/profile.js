4d4cbfc94ee81cf2700f8759fc23480181b41cec
Alexander Ignácz <ignaczalexander@gmail.com>
2019-04-28 15:37:42 +0200

    basic checks for profile page

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Spinner from '../common/Spinner'
 import { connect } from 'react-redux'
 import { getCurrentProfile, getProfileById } from '../../actions/profileAction'
+import { Link } from 'react-router-dom';
 
 class Profile extends Component {
 
@@ -9,17 +11,37 @@ class Profile extends Component {
     const { id } = this.props.match.params
     if (id) {
       console.log('by id')
-      //this.props.getProfileById(id)
+      this.props.getProfileById(id)
     }
     else {
       console.log('current');
-      //this.props.getCurrentProfile()
+      this.props.getProfileById(this.props.auth.user.id)
     }
   }
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+    if (loading) {
+      console.log('loading')
+      profileContent = <Spinner />
+    }
+    else {
+      if (!profile) {
+        profileContent = (
+          <div>
+            <h1>You dont have a profile. Create one!</h1>
+            <Link to='/create-profile'>Create your profile</Link>
+          </div>
+        )
+      }
+      else {
+        profileContent = <p>Profile is loaded, will display it soon</p>
+      }
+    }
     return (
       <div>
         Profile page
+        {profileContent}
       </div>
     )
   }
@@ -27,11 +49,13 @@ class Profile extends Component {
 Profile.propTypes = {
   profile: PropTypes.object.isRequired,
   errors: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
   getCurrentProfile: PropTypes.func.isRequired,
   getProfileById: PropTypes.func.isRequired,
 }
 const mapStateToProps = state => ({
   profile: state.profile,
-  errors: state.errors
+  errors: state.errors,
+  auth: state.auth
 })
 export default connect(mapStateToProps, { getCurrentProfile, getProfileById })(Profile)
