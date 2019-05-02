4d3acc4d7967a022a7aac84e6012d1aaeeade635
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-02 20:39:38 +0200

    basic display profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import Spinner from '../common/Spinner'
 import { connect } from 'react-redux'
 import { getCurrentProfile, getProfileById } from '../../actions/profileAction'
 import { Link } from 'react-router-dom';
+import InterestSkillList from '../project/InterestSkillList'
 
 class Profile extends Component {
 
@@ -35,7 +36,83 @@ class Profile extends Component {
         )
       }
       else {
-        profileContent = <p>Profile is loaded, will display it soon</p>
+        profileContent =
+          <div>
+            <Link to='/edit-profile'>Edit your profile</Link>
+
+            <p>{profile.firstName}</p>
+            <p>{profile.lastName}</p>
+            <p>Skills</p>
+            <InterestSkillList
+              list={profile.skills}
+              customStyle={'skill-list-item'}
+            />
+            <p>Interests</p>
+            <InterestSkillList
+              list={profile.interests}
+              customStyle={'interest-list-item'}
+            />
+            <p>Education</p>
+            <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Institute name</th>
+                                <th>Degree</th>
+                                <th>Field of study</th>
+                                <th>From</th>
+                                <th>To</th>
+                                <th>Description</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {profile.education.map((edu, i) => (
+                                <tr key={i}>
+                                    <td>{edu.instituteName}</td>
+                                    <td>{edu.degree}</td>
+                                    <td>{edu.fieldOfStudy}</td>
+                                    <td>{edu.fromDate}</td>
+                                    <td>{edu.endDate}</td>
+                                    <td>{edu.description}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+            <p>Experience</p>
+            <table className='table'>
+              <thead>
+                <tr>
+                  <th>Company name</th>
+                  <th>Title</th>
+                  <th>Location</th>
+                  <th>From</th>
+                  <th>To</th>
+                  <th>Description</th>
+                </tr>
+              </thead>
+              <tbody>
+                {profile.pastExperience.map((exp, i) => (
+                  <tr key={i}>
+                    <td>{exp.company}</td>
+                    <td>{exp.title}</td>
+                    <td>{exp.location}</td>
+                    <td>{exp.fromDate}</td>
+                    <td>{exp.endDate}</td>
+                    <td>{exp.description}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <p>Github</p>
+            <p>{profile.github}</p>
+            <p>Facebook</p>
+            <p>{profile.facebook}</p>
+            <p>Linkedin</p>
+            <p>{profile.linkedin}</p>
+            <p>Twitter</p>
+            <p>{profile.twitter}</p>
+            <p>Instagram</p>
+            <p>{profile.instagram}</p>
+          </div>
       }
     }
     return (
