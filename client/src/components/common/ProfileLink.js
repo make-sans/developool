8f242a4f3305adb87ecb839b4177fb8155f6586e
Stefan Joemagi <stefan.joemagi@gmail.com>
2019-05-03 12:49:52 +0300

    Component for links on profile page

---
+++ client/src/components/common/ProfileLink.js
@@ -0,0 +1,23 @@
+import React from "react";
+import propTypes from 'prop-types';
+
+const ProfileLink = ({ iconName, name, value }) => {
+    return (
+        <div className="profile-field-group">
+            <div className="d-inline-flex align-items-center">
+                <i className={`fab fa-2x ${iconName}`}></i>
+                <h4 className="px-2 m-0"> {name} </h4>
+                {value}
+            </div>
+        </div>
+    );
+}
+
+// require all properties
+ProfileLink.propTypes = {
+    iconName: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+    value: propTypes.string.isRequired,
+};
+
+export default ProfileLink;
