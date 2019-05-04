ed108ef3d531db883008d4e6a2c0ba63b022ce43
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-04 17:14:25 +0200

    profile style changes

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -20,6 +20,7 @@ class Profile extends Component {
       this.props.getProfileById(this.props.auth.user.id)
     }
   }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
@@ -46,22 +47,28 @@ class Profile extends Component {
 
                 <div className="profile-field-group">
                   <h3 className="font-weight-light">Skills</h3>
-                  <InterestSkillList
+                  {profile.skills.length > 0 ? (<InterestSkillList
                     list={profile.skills}
                     customStyle={'skill-list-item'}
-                  />
+                  />) : (
+                      <p className="text-muted">No skills added</p>
+                    )}
+
                 </div>
 
                 <div className="profile-field-group">
                   <h3 className="font-weight-light">Interests</h3>
-                  <InterestSkillList
+                  {profile.interests.length > 0 ? (<InterestSkillList
                     list={profile.interests}
                     customStyle={'interest-list-item'}
-                  />
+                  />) : (
+                      <p className="text-muted">No interests added</p>
+                    )}
+
                 </div>
                 <div className="profile-field-group">
                   <h3 className="font-weight-light">Education</h3>
-                  <table className='table'>
+                  {profile.education.length > 0 ? (<table className='table'>
                     <thead>
                       <tr>
                         <th>Institute name</th>
@@ -84,37 +91,45 @@ class Profile extends Component {
                         </tr>
                       ))}
                     </tbody>
-                  </table>
+                  </table>) : (
+                      <p className="text-muted">No education added</p>
+                    )}
+
                 </div>
 
                 <div className="profile-field-group">
                   <h3 className="font-weight-light">Experience</h3>
-                  <table className='table'>
-                    <thead>
-                      <tr>
-                        <th>Company name</th>
-                        <th>Title</th>
-                        <th>Location</th>
-                        <th>From</th>
-                        <th>To</th>
-                        <th>Description</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {profile.pastExperience.map((exp, i) => (
-                        <tr key={i}>
-                          <td>{exp.company}</td>
-                          <td>{exp.title}</td>
-                          <td>{exp.location}</td>
-                          <td>{exp.fromDate}</td>
-                          <td>{exp.endDate}</td>
-                          <td>{exp.description}</td>
+                  {profile.pastExperience.length > 0 ? (
+                    <table className='table'>
+                      <thead>
+                        <tr>
+                          <th>Company name</th>
+                          <th>Title</th>
+                          <th>Location</th>
+                          <th>From</th>
+                          <th>To</th>
+                          <th>Description</th>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                      </thead>
+                      <tbody>
+                        {profile.pastExperience.map((exp, i) => (
+                          <tr key={i}>
+                            <td>{exp.company}</td>
+                            <td>{exp.title}</td>
+                            <td>{exp.location}</td>
+                            <td>{exp.fromDate}</td>
+                            <td>{exp.endDate}</td>
+                            <td>{exp.description}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  ) : (
+                      <p className="text-muted">No past experience added</p>
+                    )}
+
                 </div>
-                
+
                 {/* Profile links */}
                 <ProfileLink name="Github" iconName="fa-github" value={profile.github || "None"} />
                 <ProfileLink name="Facebook" iconName="fa-facebook-square" value={profile.facebook || "None"} />
