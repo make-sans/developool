1ff20038c4aa83ea2b61df2615978bb65744e24d
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-08 20:08:55 +0200

    changed links to navLinks

--- client/src/components/common/Navbar.js
+++ client/src/components/common/Navbar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { logoutUser } from '../../actions/authActions';
 
 export class Navbar extends Component {
@@ -11,38 +11,38 @@ export class Navbar extends Component {
     if (this.props.auth.isAuthenticated) {
       return [
         <li className="nav-item" key="my-projects">
-          <Link className="nav-link" to="/my-projects">
+          <NavLink className="nav-link" to="/my-projects">
             My projects
-        </Link>
+          </NavLink>
         </li>,
         <li className="nav-item" key="secret">
-          <Link className="nav-link" to="/create-project">
+          <NavLink className="nav-link" to="/create-project">
             Create Project
-          </Link>
+          </NavLink>
         </li>,
         <li className="nav-item" key="profile">
-          <Link className="nav-link" to="/profile">
+          <NavLink className="nav-link" to="/profile">
             Profile
-          </Link>
+          </NavLink>
         </li>,
-        <li className="nav-item" key="signout" >
-          <button className="btn nav-link" onClick={this.props.logoutUser}>
+        <li className="nav-item" key="signout">
+          <span className="nav-link hover" onClick={this.props.logoutUser}>
             Sign out
             <i className="fas fa-sign-out-alt px-2" />
-          </button>
+          </span>
         </li>
       ];
     }
     return [
       <li className="nav-item" key="signin">
-        <Link className="nav-link" to="/login">
+        <NavLink className="nav-link" to="/login">
           Login
-        </Link>
+        </NavLink>
       </li>,
       <li className="nav-item" key="signup">
-        <Link className="nav-link" to="/register">
+        <NavLink className="nav-link" to="/register">
           Register
-        </Link>
+        </NavLink>
       </li>
     ];
   }
@@ -50,9 +50,9 @@ export class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-        <Link className="navbar-brand" to="/">
+        <NavLink className="navbar-brand" to="/">
           4th Sem Project
-        </Link>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
@@ -67,12 +67,28 @@ export class Navbar extends Component {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li key="projects" className="nav-item">
-              <Link className="nav-link" to="/projects">
-                Projects
-          </Link>
+            <li key="home" className="nav-item">
+              <NavLink
+                className="nav-link"
+                exact
+                to="/"
+                activeClassName="active"
+              >
+                Home
+              </NavLink>
             </li>
-            {this.navbarLinks()}</ul>
+            <li key="projects" className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/projects"
+                activeClassName="active"
+              >
+                Projects
+              </NavLink>
+            </li>
+
+            {this.navbarLinks()}
+          </ul>
         </div>
       </nav>
     );
