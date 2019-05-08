0abf6dc15cc94ca9d6c7530bbbc2d6ff9c25f1e7
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-08 13:35:08 +0200

    check for authentication every time

--- client/src/components/common/PrivateRoute.js
+++ client/src/components/common/PrivateRoute.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
 import PropTypes from 'prop-types';
+import jwt_decode from 'jwt-decode';
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
-  <Route
-    {...rest}
-    render={props =>
-      auth.isAuthenticated === true ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
+const PrivateRoute = ({ component: Component, auth, logoutUser, ...rest }) => {
+  let isAuthenticated = false;
+  //check for token
+  if (localStorage.jwtToken) {
+    const decoded = jwt_decode(localStorage.jwtToken);
+    const currentTime = Date.now() / 1000;
+    isAuthenticated = auth.isAuthenticated;
+
+    if (decoded.exp < currentTime) {
+      //logout
+      logoutUser();
+      isAuthenticated = false;
     }
-  />
-);
+  } else {
+    //logout
+    logoutUser();
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
 PrivateRoute.propTypes = {
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  logoutUser: PropTypes.func.isRequired
 };
 const mapStateToProps = state => ({
   auth: state.auth
 });
-export default connect(mapStateToProps)(PrivateRoute);
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(PrivateRoute);
