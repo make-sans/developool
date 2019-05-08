1ffcdd2bf512f707bea2beb0868b21fe5b6bc99b
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-08 16:42:53 +0200

    dont display url

--- client/src/components/common/ProfileLink.js
+++ client/src/components/common/ProfileLink.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React from 'react';
 import propTypes from 'prop-types';
 
 const ProfileLink = ({ iconName, name, value }) => {
-    return (
-        <div className="profile-field-group">
-            <div className="d-inline-flex align-items-center">
-                <i className={`fab fa-2x ${iconName}`}></i>
-                <h4 className="px-2 m-0"> {name} </h4>
-                <a href={value} target="_blank" rel="noopener noreferrer">{value}</a>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="profile-field-group">
+      <div className="d-inline-flex align-items-center">
+        <i className={`fab fa-2x ${iconName}`} />
+        <a
+          href={value}
+          className="px-2 m-0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {name}
+        </a>
+      </div>
+    </div>
+  );
+};
 
 // require all properties
 ProfileLink.propTypes = {
-    iconName: propTypes.string.isRequired,
-    name: propTypes.string.isRequired,
-    value: propTypes.string.isRequired,
+  iconName: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  value: propTypes.string.isRequired
 };
 
 export default ProfileLink;
