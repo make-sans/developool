4a210f6db472325a95fa3a80e403c286f733eedd
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-10 21:08:03 +0200

    project card responsive

--- client/src/components/project/ProjectCard.js
+++ client/src/components/project/ProjectCard.js
@@ -7,15 +7,19 @@ export default function ProjectCard(props) {
   return (
     <div className="card mb-4 shadow-sm rounded" key={project._id}>
       <div className="card-body">
-        <div className="d-flex align-items-center">
-          <h3 className="font-weight-lighter mr-2">{project.title}</h3>
-          <p className="text-muted m-0 mr-1">· by</p>
-          <Link
-            className={'text-muted m-0 mr-3'}
-            to={`/profile/${project.owner.id}`}
-          >
-            {project.owner.username}
-          </Link>
+        <div className="row align-items-center px-3 mb-2">
+          <div className="col-auto d-flex p-0">
+            <h3 className="font-weight-lighter mr-2">{project.title}</h3>
+          </div>
+          <div className="col-auto d-flex p-0">
+            <p className="text-muted m-0 mr-1">· by</p>
+            <Link
+              className={'text-muted m-0 mr-3'}
+              to={`/profile/${project.owner.id}`}
+            >
+              {project.owner.username}
+            </Link>
+          </div>
         </div>
         <p className="card-text">{project.publicDescription}</p>
         <InterestSkillList
