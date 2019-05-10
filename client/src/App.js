a8a28ec495a90cc64ff219e5b087745a49a35b42
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-10 18:40:44 +0200

    redesigned landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,89 @@
 import React, { Component } from 'react';
+import mainIll from './assets/img/undraw/undraw_programmer_imem.svg';
+import joinProjectIll from './assets/img/undraw/undraw_mobile_testing_reah.svg';
+import exploreProjectIll from './assets/img/undraw/undraw_search_2dfv.svg';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
+  onRegisterButtonClick = () => {
+    this.props.history.push('/register');
+  };
   render() {
+    const { isAuthenticated } = this.props.auth;
+
+    // if (isAuthenticated) {
+    //   return (
+    //     <div className="container">
+    //       <div className="row mt-4">
+    //         <div className="col-md-">
+    //           <h1>Welcome</h1>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   );
+    // }
     return (
       <div className="App">
-        <div className="row justify-content-center mt-4">
-          <div className="col-7 text-center">
-            <h2 className="display-4">Welcome to [4th-semester-project]</h2>
-            <p className="lead">The palace where ideas meet developers</p>
+        <div className="row my-5 p-3">
+          <div className="col-md-5 offset-md-1">
+            <h1>The palace where ideas meet developers</h1>
+            <p>
+              Cras finibus ipsum non lacus euismod, in maximus sem auctor. Sed
+              quam ex, mattis sodales eros nec, consectetur elementum leo.
+              Nullam varius ultricies justo, et egestas risus tempor vitae.
+              Nulla in turpis tristique, iaculis ex vel, tempor purus.
+            </p>
+          </div>
+          <div className="col-md-6">
+            <img className="img-fluid" src={mainIll} alt="" />
           </div>
         </div>
-        <div className="row justify-content-center mt-5 text-white bg-dark p-3">
-          <div className="col-md-3 text-center">
-            <i className="far fa-lightbulb fa-2x p-3" />
-            <p>Explore ideas</p>
+        <div className="row justify-content-around p-6 skew bg-light align-items-center">
+          <div className="col-md-5 text-center skew-content">
+            <img className="h-auto w-50" src={joinProjectIll} alt="" />
           </div>
-          <div className="col-md-3 text-center">
-            <i className="fas fa-share-alt fa-2x p-3" />
-            <p>Share your idea</p>
-          </div>
-          <div className="col-md-3 text-center">
-            <i className="fas fa-users fa-2x p-3" />
-            <p>Join a project</p>
+          <div className="col-md-5 skew-content">
+            <h3 className="semibold">Join a project</h3>
+            <p>
+              Cras finibus ipsum non lacus euismod, in maximus sem auctor. Sed
+              quam ex, mattis sodales eros nec, consectetur elementum leo.
+              Nullam varius ultricies justo, et egestas risus tempor vitae.
+              Nulla in turpis tristique, iaculis ex vel, tempor purus.
+            </p>
           </div>
         </div>
+        <div className="row justify-content-around p-6">
+          <div className="col-md-5 py-4">
+            <h3 className="semibold">Explore project ideas</h3>
+            <p>
+              Cras finibus ipsum non lacus euismod, in maximus sem auctor. Sed
+              quam ex, mattis sodales eros nec, consectetur elementum leo.
+              Nullam varius ultricies justo, et egestas risus tempor vitae.
+              Nulla in turpis tristique, iaculis ex vel, tempor purus.
+            </p>
+          </div>
+          <div className="col-md-5 text-center">
+            <img className="h-auto w-50" src={exploreProjectIll} alt="" />
+          </div>
+        </div>
+        {!isAuthenticated && (
+          <div className="row justify-content-center mb-5">
+            <div className="col-auto">
+              <button
+                onClick={this.onRegisterButtonClick}
+                className="btn btn-register"
+              >
+                Register now
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
-
-export default App;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(mapStateToProps)(App);
