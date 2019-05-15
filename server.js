7d594326af97b67593e173cd03362737582edbfe
Alexander Ignácz <ignaczalexander@gmail.com>
2019-05-15 11:30:50 +0200

    serve static index file

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const auth = require('./routes/api/auth');
 const register = require('./routes/api/registration');
 const project = require('./routes/api/project');
 const profile = require('./routes/api/profile');
+const path = require('path');
 
 const app = express();
 app.use(cors());
@@ -26,6 +27,13 @@ app.use('/api/accounts', accounts);
 app.use('/api/auth', auth);
 app.use('/api/project', project);
 app.use('/api/profile', profile);
+
+// Production part
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(config.get('serverPort'), () =>
   console.log(`Server started on port ${config.get('serverPort')}`)
