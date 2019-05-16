50969464a62357d95cfaf594ab23314a4fc0c379
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2019-05-16 12:46:06 +0200

    Server accepts everything from static files

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/api/profile', profile);
 // Production part
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
