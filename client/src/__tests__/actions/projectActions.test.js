295451da23b083f9e1350762811aa6be7eedaac7
Stefan Joemagi <stefan.joemagi@gmail.com>
2019-05-22 18:59:27 +0200

    Change the test cases

--- client/src/__tests__/actions/projectActions.test.js
+++ client/src/__tests__/actions/projectActions.test.js
@@ -4,29 +4,33 @@ import thunk from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import * as types from '../../actions/types'
 import * as actions from '../../actions/projectActions';
+import { API_URL } from '../../constants/index';
 
 const middlewares = [thunk]
 const mockStore = configureMockStore(middlewares)
-const mock = new MockAdapter(mockAxios);
+let mock;
+let store;
 
 describe('Project Actions', () => {
+    beforeEach(() => {
+        mock = new MockAdapter(mockAxios)
+        store = mockStore({})
+      })
     afterEach(() => {
         mock.restore()
     });
 
     // get the project that shouldn't exist
-    it('creates GET_PROJECT with payload null when trying to get a project with id that doesn\'t exist', (done) => {
+    it('creates GET_PROJECT with payload null when getting a project that doesn\'t exist', (done) => {
         // fake id
         const id = "fid1";
-        // mock the axios call of the action
-        mock.onGet('/api/project/'+id).reply(404, {}, {'Authorization':'auth_key'});
+        // mock the axios call of the action, intercepting any calls made to this api path and returning what we specify.
+        mock.onGet(API_URL+'/project/'+id).reply(404, {'msg' : 'given project does not exist'});
         // expected actions
         const expectedActions = [
             { type: types.PROJECT_LOADING },
             { type: types.GET_PROJECT, payload: null }
         ];
-
-        const store = mockStore();
 
         store.dispatch(actions.getProject(id));
         // need to add a timeout because without it, since the action is synchronous the first dispatch(action called) 
@@ -37,5 +41,27 @@ describe('Project Actions', () => {
             done();
         }, 1000);
 
+    });
+
+    // get a project that exists
+    it('creates GET_PROJECT with payload value as the project\'s fields when getting a project that exists', (done) => {
+        // project to return
+        const tempId = 'id1';
+        const proj = {'_id' : tempId};
+
+        // intercept the api call and mock the api server
+        mock.onGet(`${API_URL}/project/${tempId.toString()}`).reply(200, {proj});
+        
+        const expectedActions = [
+            { type: types.PROJECT_LOADING },
+            { type: types.GET_PROJECT, payload: {proj}}
+        ];
+
+        store.dispatch(actions.getProject(tempId));
+        
+        setTimeout(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+            done();
+        }, 1000);
     });
 });
